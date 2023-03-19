f1692b1ab9a6e5765e0844ea2911b69d1815161d
dmitrii_stafeev <stafeeff,dmitrij@yandex,ru>
2023-03-19 17:09:16 +0300

    integration of template layout, customization of the main page template

---
+++ megano/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class MainView(View):
+    """ Тестовая главная страница """
+
+    def get(self, request):
+        return render(request, 'index.html')
