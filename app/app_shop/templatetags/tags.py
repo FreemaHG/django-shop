1f0a11370097ff032410427cd9abba4f80ce0639
dmitrii_stafeev <stafeeff,dmitrij@yandex,ru>
2023-05-05 15:05:31 +0300

    task 15.3. Fix paginate with filtered and sorted products

--- app/app_shop/templatetags/tags.py
+++ app/app_shop/templatetags/tags.py
@@ -20,18 +20,50 @@ def output_tags():
 
 
 @register.simple_tag
-def clear_link(link: str) -> str:
+def clear_link_for_sort(link: str) -> str:
     """
     Очистка входящей строки от текста после ключевой фразы 'sort'.
     Добавление спец.символа в конце для корректного парсинга параметров из URL.
     """
+    # logger.info(f'Очистка ссылки для сортировки: {link}')
+
     if '&sort' in link:
         link = link.split('&sort')[0]
 
     elif '?sort' in link:
         link = link.split('?sort')[0]
 
-    logger.debug(f'Возврат очищенной ссылки: {link}')
+    elif '&page=' in link:
+        link = link.split('&page=')[0]
+
+    elif '?page=' in link:
+        link = link.split('?page=')[0]
+
+    # logger.debug(f'Возврат очищенной ссылки: {link}')
+
+    if '?' not in link:
+        link += '?'
+    else:
+        link += '&'
+
+    return link
+
+
+@register.simple_tag
+def clear_link_for_paginate(link: str) -> str:
+    """
+    Очистка входящей строки от текста после ключевой фразы 'page'.
+    Добавление спец.символа в конце для корректного парсинга параметров из URL.
+    """
+    # logger.info(f'Очистка ссылки для пагинации: {link}')
+
+    if '&page=' in link:
+        link = link.split('&page=')[0]
+
+    elif '?page=' in link:
+        link = link.split('?page=')[0]
+
+    # logger.debug(f'Возврат очищенной ссылки: {link}')
 
     if '?' not in link:
         link += '?'
